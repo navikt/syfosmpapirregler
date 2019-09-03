ecb0071ea34e8d6bdb9e8211e0289a4a0a7e89e8
andreasnilsen <danduras@gmail.com>
2019-09-03 16:17:25 +0200

    get env variable

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -5,12 +5,12 @@ import java.lang.RuntimeException
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val jwkKeysUrl: String = getEnvVar("JWKKEYS_URL", "https://login.microsoftonline.com/common/discovery/keys"),
-    val jwtIssuer: String = getEnvVar("JWT_ISSUER"),
     val clientId: String = getEnvVar("CLIENT_ID"),
+    val jwtIssuer: String = getEnvVar("JWT_ISSUER"),
     val appIds: List<String> = getEnvVar("ALLOWED_APP_IDS")
         .split(",")
         .map { it.trim() }
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
-    defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
+    System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
