02609b3f82812b50527d24dad9047871bf844d62
andreasnilsen <danduras@gmail.com>
2019-09-05 13:15:37 +0200

    Log in authentication

--- src/main/kotlin/no/nav/syfo/authentication/Authentication.kt
+++ src/main/kotlin/no/nav/syfo/authentication/Authentication.kt
@@ -7,6 +7,10 @@ import io.ktor.auth.Authentication
 import io.ktor.auth.jwt.JWTPrincipal
 import io.ktor.auth.jwt.jwt
 import no.nav.syfo.Environment
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+val log: Logger = LoggerFactory.getLogger("no.nav.syfo.smpapirregler.authentication")
 
 fun Application.setupAuth(environment: Environment, jwkProvider: JwkProvider) {
     install(Authentication) {
@@ -14,9 +18,12 @@ fun Application.setupAuth(environment: Environment, jwkProvider: JwkProvider) {
             verifier(jwkProvider, environment.jwtIssuer)
             validate { credentials ->
                 val appId: String = credentials.payload.getClaim("appid").asString()
+                log.info("authorization attempt for $appId")
                 if (appId in environment.appIds && environment.clientId in credentials.payload.audience) {
+                    log.info("authorization ok")
                     return@validate JWTPrincipal(credentials.payload)
                 }
+                log.info("authorization failed")
                 return@validate null
             }
         }
