4c5650a92d8dce817a6bc53b4dffc1fddeb7f1e0
Andreas Sagen Aspaas <danduras@gmail.com>
2023-10-02 16:05:33 +0200

    wip [skip ci]

---
+++ src/main/kotlin/no/nav/syfo/papirsykemelding/rules/patientageover70/PatientAgeOver70RuleHit.kt
@@ -0,0 +1,20 @@
+package no.nav.syfo.rules.patientageover70
+
+import no.nav.syfo.model.Status
+import no.nav.syfo.papirsykemelding.rules.common.RuleHit
+import no.nav.syfo.rules.common.RuleHit
+
+enum class PatientAgeOver70RuleHit(
+    val ruleHit: RuleHit,
+) {
+    PASIENT_ELDRE_ENN_70(
+        ruleHit =
+            RuleHit(
+                rule = "PASIENT_ELDRE_ENN_70",
+                status = Status.MANUAL_PROCESSING,
+                messageForSender =
+                    "Pasienten er over 70 år. Sykmelding kan ikke benyttes. Pasienten har fått beskjed.",
+                messageForUser = "Sykmelding kan ikke benyttes etter at du har fylt 70 år",
+            ),
+    ),
+}
